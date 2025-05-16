05a64ecc15bbb8747345a0ba5e4049768556e536
suryakanta007 <sahusuryakanta073@gmail.com>
2025-05-16 12:46:00 +0530

    add user routes set up and set global error handler with constom api error and response and asynshandler.

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,4 +1,6 @@
 import express from "express"
+import userRoutes from "./routes/user.routes.js"
+import errorHandler from "./middlewares/error.middleware.js";
 const app = express();
 
 app.use(express.json());
@@ -7,5 +9,8 @@ app.use(express.urlencoded({extended:true}));
 app.get("/",(req,res)=>{
     res.send("Hello , Wellcome to LeetLab.")
 })
+
+app.use("/api/v1/auth",userRoutes);
+app.use(errorHandler);
 
 export default app;
---
+++ Backend/src/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+const router = Router();
+
+// router.route("/register").post()
+// router.route("/login").post()
+// router.route("logout").post()
+// router.route("/profile").get()
+
+export default router;
