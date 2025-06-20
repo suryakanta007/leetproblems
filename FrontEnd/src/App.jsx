ad702099d35aafe9d7e3fba2e4b9bb9ab3c1632a
suryakanta007 <sahusuryakanta073@gmail.com>
2025-06-20 23:05:16 +0530

    add basing routing for login amd signup with react router dom.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
+import { Routes,Route,Navigate } from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import SignupPage from './pages/SignupPage'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <div>App</div>
+   <>
+    <div className='flex flex-col items-center justify-center'>
+    <Routes>
+      <Route
+      path='/login'
+      element={<LoginPage/>}
+      />
+      <Route
+      path='signup'
+      element={<SignupPage/>}
+      />
+      <Route
+      path='/'
+      element={<HomePage/>}
+      />
+    </Routes>
+    </div>
+    
+   </>
   )
 }
 
