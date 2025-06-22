2b45aa6c77003bf75ef45be23001dcd5eba7d866
suryakanta007 <sahusuryakanta073@gmail.com>
2025-06-22 22:58:52 +0530

    add layout and navbar.

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -1,22 +1,23 @@
-import React,{useEffect} from 'react'
-import { Routes,Route,Navigate } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import LoginPage from './pages/LoginPage'
 import SignupPage from './pages/SignupPage'
 import HomePage from './pages/HomePage'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
 import { useAuthStore } from './store/useAuthStore'
 import { Loader } from 'lucide-react'
+import Layout from './layout/Layout'
 
 const App = () => {
 
-  const {authUser,checkAuth,ischeckingAuth}= useAuthStore();
+  const { authUser, checkAuth, ischeckingAuth } = useAuthStore();
 
-  useEffect(()=>{
+  useEffect(() => {
     checkAuth()
-  },[checkAuth])
-  console.log("Auth User : ",authUser);
-  if(ischeckingAuth&&!authUser){
-    return(
+  }, [checkAuth])
+  console.log("Auth User : ", authUser);
+  if (ischeckingAuth && !authUser) {
+    return (
       <div className="flex items-center justify-center h-screen">
         <Loader className="size-10 animate-spin" />
       </div>
@@ -25,26 +26,29 @@ const App = () => {
 
 
   return (
-   <>
-    <div className='flex flex-col items-center justify-center'>
-    <Toaster/>
-    <Routes>
-      <Route
-      path='/login'
-      element={!authUser?<LoginPage/>:<Navigate to={"/"}/>}
-      />
-      <Route
-      path='/signup'
-      element={!authUser?<SignupPage/>:<Navigate to={"/"}/>}
-      />
-      <Route
-      path='/'
-      element={authUser?<HomePage/>:<Navigate to={"/login"}/>}
-      />
-    </Routes>
-    </div>
-    
-   </>
+    <>
+      <div className='flex flex-col items-center justify-center'>
+        <Toaster />
+        <Routes>
+          <Route path='/' element={<Layout/>}>
+            <Route
+              index
+              element={authUser ? <HomePage /> : <Navigate to={"/login"} />}
+            />
+          </Route>
+          <Route
+            path='/login'
+            element={!authUser ? <LoginPage /> : <Navigate to={"/"} />}
+          />
+          <Route
+            path='/signup'
+            element={!authUser ? <SignupPage /> : <Navigate to={"/"} />}
+          />
+
+        </Routes>
+      </div>
+
+    </>
   )
 }
 
---
+++ FrontEnd/src/components/LogoutButton.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { useAuthStore } from "../store/useAuthStore";
+import { AwardIcon } from "lucide-react";
+
+
+const LogoutButton = ({ children }) => {
+    const { logout } = useAuthStore();
+
+    const onLogout = async () => {
+        await logout();
+    }
+
+    return (
+        <button className="btn btn-primary" onClick={onLogout}>
+            {children}
+        </button>
+    )
+}
+
+export default LogoutButton
