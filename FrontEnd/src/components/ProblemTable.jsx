963e00b3527d75c53acae1666bc226c270be79e1
suryakanta007 <sahusuryakanta073@gmail.com>
2025-06-25 23:09:47 +0530

    implimenting ProblemTable and problem store for state management.

---
+++ FrontEnd/src/components/ProblemTable.jsx
@@ -0,0 +1,12 @@
+import React,{useState, useMemo} from 'react'
+import {useAuthStore} from "../store/useAuthStore"
+import { Link } from 'react-router-dom'
+import {Bookmark,PencilIcon,Trash,TrashIcon,Plus} from "lucide-react"
+
+const ProblemTable = ({problems}) => {
+  return (
+    <div>ProblemTable</div>
+  )
+}
+
+export default ProblemTable
---
+++ FrontEnd/src/store/useProblemStore.js
@@ -0,0 +1,56 @@
+import {create} from 'zustand'
+import {axiosInstance} from '../lib/axios.js'
+import {toast} from 'react-hot-toast'
+import { AwardIcon } from 'lucide-react'
+
+export const  useProblemStore  = create((set)=>({
+    problems:[],
+    problem:null,
+    solvedProblems:[],
+    isProblemsLoading:false,
+    isProblemLoading:false,
+
+    getAllProblems:async ()=>{
+        try {
+            set({isProblemsLoading:true})
+            const res = await axiosInstance.get("/problems/");
+            set({problems:res.data.data});
+        } catch (error) {
+            console.log("Error geting all Problems: ",error);
+            toast.error("Error in geting problems")
+        }
+        finally{
+            set({isProblemsLoading:false})
+        }
+    },
+    getProblemById:async(id)=>{
+        try {
+            set({isProblemsLoading:true})
+            const res = await axiosInstance.get(`/problems/get-problem/${id}`)
+            set({problems:res.data.data})
+        } catch (error) {
+            console.log("Erro in getProblemById ",error);
+            toast.error("Error in getProblemById .")
+            
+        }finally{
+            set({isProblemsLoading:false})
+        }
+    },
+
+    getSolvedProblemByUser :async ()=>{
+        try {
+            const res = await axiosInstance.get("/problems/get-solved-problems")
+
+            set({solvedProblems:res.data.data});
+            toast.success(res.data.message||"Solved Problems are fetched successfully.");
+        } catch (error) {
+            console.log("Error in getSolvedProblemByUser ",error);
+            toast.error("Error in getSolvedProblemByUser .")
+        }
+    }
+
+    
+
+
+
+})) 
