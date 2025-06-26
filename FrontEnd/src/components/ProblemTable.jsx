cb9db8c637a8f269f8d0baa400280e05151a98af
suryakanta007 <sahusuryakanta073@gmail.com>
2025-06-26 22:53:10 +0530

    update to all and sync all the work update.

--- FrontEnd/src/components/ProblemTable.jsx
+++ FrontEnd/src/components/ProblemTable.jsx
@@ -8,10 +8,19 @@ import {Bookmark,PencilIcon,Trash,TrashIcon,Plus} from "lucide-react"
 const ProblemTable = ({problems}) => {
 
     const {authUser} = useAuthStore();
-    const [search,setSearch] = useState(" ");
+    const [search,setSearch] = useState("");
+
+    
     const [difficulty,setDifficulty] = useState("ALL");
     const [selectedTag, setSelectedTag] = useState("ALL");
     const [currentPage,setCurrentPage] = useState(1);
+    const difficulties = ["EASY","MEDIUM","HARD"];
+
+  const allTags =useMemo(()=>{
+    if(!Array.isArray(problems)) return [];
+    const tagsSet = new Set();
+  })
+
   return (
     <div className='w-full max-w-6xl mx-auto mt-10 '>
         <div className="flex justify-between items-center mb-6">
@@ -24,6 +33,41 @@ const ProblemTable = ({problems}) => {
           Create Playlist
         </button>
       </div>
+
+      <div className="flex flex-wrap justify-between items-center mb-6 gap-4">
+      <input
+          type="text"
+          placeholder="Search by title"
+          className="input input-bordered w-full md:w-1/3 bg-base-200"
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <select
+          className="select select-bordered bg-base-200"
+          value={difficulty}
+          onChange={(e) => setDifficulty(e.target.value)}
+        >
+          <option value="ALL">All Difficulties</option>
+          {difficulties.map((diff) => (
+            <option key={diff} value={diff}>
+              {diff.charAt(0).toUpperCase() + diff.slice(1).toLowerCase()}
+            </option>
+          ))}
+        </select>
+        <select
+          className="select select-bordered bg-base-200"
+          value={selectedTag}
+          onChange={(e) => setSelectedTag(e.target.value)}
+        >
+          <option value="ALL">All Tags</option>
+          {allTags.map((tag) => (
+            <option key={tag} value={tag}>
+              {tag}
+            </option>
+          ))}
+        </select>
+      </div>
+
     </div>
   )
 }
